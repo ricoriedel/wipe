243a01a638803c81947bb4abbd477cad18bd4a36
Nicolas <>
2022-04-03 14:52:20 +0200

    Add test

--- src/fill/level.rs
+++ src/fill/level.rs
@@ -14,3 +14,16 @@ impl FillMode for LevelFillMode {
         level
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        let mode = LevelFillMode::new();
+
+        assert_eq!(0.3, mode.sample(0.3, Vector::ZERO));
+        assert_eq!(0.7, mode.sample(0.7, Vector::new(0.1, 0.2)));
+    }
+}
