f5730be8354e0303167623ead4ef1d190dbfa93a
Rico Riedel <rico.riedel@protonmail.ch>
2022-08-01 11:46:07 +0200

    Replace sampler

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+pub mod convert;
 pub mod error;
 pub mod pattern;
 pub mod printer;
-pub mod convert;
 pub mod term;
 mod vec;
-mod sampler;
 
 fn main() -> Result<(), error::Error> {
     Ok(())
--- src/pattern/mod.rs
+++ src/pattern/mod.rs
@@ -21,3 +21,80 @@ pub trait PatternFactory {
 pub trait Pattern {
     fn sample(&self, pos: Vector) -> f32;
 }
+
+pub trait SamplerFactory {
+    fn create(&self, config: &Config) -> Box<dyn Sampler>;
+}
+
+#[cfg_attr(test, mockall::automock)]
+pub trait Sampler {
+    fn char(&self, pos: Vector) -> f32;
+
+    fn color(&self, pos: Vector) -> f32;
+}
+
+#[derive(derive_more::Constructor)]
+pub struct SamplerFactoryImpl {
+    char: Box<dyn PatternFactory>,
+    color: Box<dyn PatternFactory>,
+}
+
+#[derive(derive_more::Constructor)]
+pub struct SamplerImpl {
+    char: Box<dyn Pattern>,
+    color: Box<dyn Pattern>,
+}
+
+impl SamplerFactory for SamplerFactoryImpl {
+    fn create(&self, config: &Config) -> Box<dyn Sampler> {
+        Box::new(SamplerImpl::new(
+            self.char.create(config),
+            self.color.create(config),
+        ))
+    }
+}
+
+impl Sampler for SamplerImpl {
+    fn char(&self, pos: Vector) -> f32 {
+        self.char.sample(pos)
+    }
+
+    fn color(&self, pos: Vector) -> f32 {
+        self.color.sample(pos)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use mockall::predicate::eq;
+
+    #[test]
+    fn char() {
+        let mut char = MockPattern::new();
+        let color = MockPattern::new();
+
+        char.expect_sample()
+            .with(eq(Vector::new(2.0, 5.0)))
+            .return_const(2.5);
+
+        let sampler = SamplerImpl::new(Box::new(char), Box::new(color));
+
+        assert_eq!(2.5, sampler.char(Vector::new(2.0, 5.0)));
+    }
+
+    #[test]
+    fn color() {
+        let char = MockPattern::new();
+        let mut color = MockPattern::new();
+
+        color
+            .expect_sample()
+            .with(eq(Vector::new(4.0, 2.0)))
+            .return_const(3.2);
+
+        let sampler = SamplerImpl::new(Box::new(char), Box::new(color));
+
+        assert_eq!(3.2, sampler.color(Vector::new(4.0, 2.0)));
+    }
+}
